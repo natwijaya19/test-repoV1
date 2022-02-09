8877abbddd438046b3321b97cee10745b5ae2a74
natwijaya19 <72418586+natwijaya19@users.noreply.github.com>
2022-02-10 00:04:27 +0700

    Update

---
+++ unitTest/SampleTest.m
@@ -0,0 +1,10 @@
+classdef SampleTest < matlab.unittest.TestCase
+    properties (TestParameter)
+        Number = {1,2,'3',4,5};
+    end
+    methods(Test)
+        function testDouble(testCase,Number)
+            testCase.verifyClass(Number,'double')
+        end
+    end
+end
